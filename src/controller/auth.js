cf89cbcc595305cf2dc850079a69560392d6bf69
umutplvn <umutpehlivan2078@gmail.com>
2024-02-18 06:52:30 +0000

    up

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -26,7 +26,7 @@ module.exports = {
           }else{
             res.send({
               error:true,
-              user,
+              result:user,
               message:"You need to verify your account first."
             })
           }
